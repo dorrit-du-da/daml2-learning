c72368a7a4426b89d6bb73346e131c4ebb6cf2cb
Judy Wu <judy.wu@digitalasset.com>
2022-05-03 09:39:14 +0800

    remove backup folder

--- ui/daml.backup.js/1041d7154af4316a5dfda1494bb260a54286b720adfd847549ff32694785580c/lib/DA/index.d.ts
+++
@@ -1,4 +0,0 @@
-import * as Random from './Random';
-export { Random } ;
-import * as Stack from './Stack';
-export { Stack } ;
--- ui/daml.backup.js/1041d7154af4316a5dfda1494bb260a54286b720adfd847549ff32694785580c/lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-import * as DA from './DA';
-export { DA } ;
-export declare const packageId = '1041d7154af4316a5dfda1494bb260a54286b720adfd847549ff32694785580c';
